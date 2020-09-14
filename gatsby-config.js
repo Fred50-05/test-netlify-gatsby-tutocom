40ccf9539c5648a2df1d8f7310469a781443d3ca
Fred50-05 <freddurel50@gmail.com>
2020-09-14 13:57:41 +0200

    ajout plugin contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `exlfzmtf7rxl`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
